5672832e50d9d9aa806989bfd9dff51615549ec1
mhi1627 <pvg1627@gmail.com>
2025-03-05 23:18:25 +0530

    Practice state updating

--- Small Projects/Trafic Light/src/App.jsx
+++ Small Projects/Trafic Light/src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function App() {
-  const [walk, setWalk] = useState(true);
-
-  function handleClick() {
-    alert(walk ? "Stop is Next" : "Walk is next")
-    setWalk(!walk);
-  }
+function App() {
+  let count = 0; 
 
   return (
-    <>
-      <button onClick={handleClick}>
-        Change to {walk ? 'Stop' : 'Walk'}
-      </button>
-      <h1 style={{
-        color: walk ? 'darkgreen' : 'darkred'
-      }}>
-        {walk ? 'Walk' : 'Stop'}
-      </h1>
-    </>
-  )
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => count = count + 1}>Increase</button>
+    </div>
+  );
 }
+
+export default App;
